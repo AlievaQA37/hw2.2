74fda71dad35794f489be58cda23042e873e2f6d
Chloe Alieva <chloe@email.com>
2022-07-14 13:19:29 +0300

    Расчет начисления бонусов v2.0

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 200 ;
-        int refill = 100 ;
-        int priceBonus = 100 ;
+        int balance = 200;
+        int refill = 100;
+        int priceBonus = 100;
 
-        int bonus = refill / priceBonus ;
-        int total = balance + refill ;
-        int creditedBonus = 0 ;
+        int bonus = refill / priceBonus;
+        int total = balance + refill;
+        int creditedBonus = 0;
 
         if (refill >= 1000) {
-            total = balance + refill + bonus ;
-            creditedBonus = bonus ;
+            total = balance + refill + bonus;
+            creditedBonus = bonus;
         }
-        
+
         System.out.println("Итоговый баланс " + total);
-        System.out.println( "Начислено " + creditedBonus + " бонусов");
-  } 
+        System.out.println("Начислено " + creditedBonus + " бонусов");
+    }
 }
